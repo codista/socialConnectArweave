fd7608e51939cfd9f8892658ef4d82c67e9ecc65
codista <nirh30@hotmail.com>
2021-12-07 14:50:09 +0200

    arweave connect

--- socialConnectAPI/src/arweave/scArweaveImpl.ts
+++ socialConnectAPI/src/arweave/scArweaveImpl.ts
@@ -1,8 +1,9 @@
 import Arweave from 'arweave';
+import Wallets from 'arweave/node/wallets';
 import dotenv from 'dotenv';
 dotenv.config();
 
-async function initArweave(): Promise<false | object> {
+async function initArweave() {
     let arweave: any,wallet: any,address: any,balance: any;
     try {
         arweave = Arweave.init({
@@ -20,14 +21,19 @@ async function initArweave(): Promise<false | object> {
         console.error("exception in initArweave",error.message);
         return false;
     }
-    return {Wallet: wallet, Address: address, Arweave: arweave};
+    return {wallet: wallet, address: address, arweave: arweave};
 }
   
 
 
 export async function follow(data: object): Promise<boolean> {
-
     
+    let ar =  await initArweave();
+    if (typeof ar == "boolean") {return false;}  
+    const {wallet, address,arweave} = ar;
+    const transaction = await ar.arweave.createTransaction({ data: "test" },wallet);
+    transaction.addTag("test-tag-1", "test-value-1");
+    await arweave.transactions.sign(transaction, wallet);
 
 
     return true;

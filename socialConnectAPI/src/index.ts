90a38b77d9ed7cbad94649e959ff95c7feafed1c
codista <nirh30@hotmail.com>
2021-12-15 11:50:20 +0200

    https

--- socialConnectAPI/src/index.ts
+++ socialConnectAPI/src/index.ts
@@ -1,8 +1,43 @@
 import app from "./app"
+import https from 'https';
+import http from 'http';
+import fs from 'fs';
+import path from 'path';
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  /* eslint-disable no-console */
-  console.log(`Listening: http://localhost:${port}`);
-  /* eslint-enable no-console */
+
+
+
+
+// Listen on http
+const httpServer = http.createServer(app);
+httpServer.listen(port, () => {
+  console.log('HTTP Server running on port '+port);
 });
+
+
+//if certificates present, listen on https
+var certPath = path.join(__dirname,'cert.crt');
+var keyPath = path.join(__dirname,'prv.key');
+
+if (fs.existsSync(certPath) && fs.existsSync(keyPath)) {
+  const httpsServer = https.createServer({
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath),
+  }, app);
+
+  httpsServer.listen(443, () => {
+    console.log('HTTPS Server running on port 443');
+});
+}
+
+
+
+
+
+
+//app.listen(port, () => {
+  /* eslint-disable no-console */
+  //console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
+//});
